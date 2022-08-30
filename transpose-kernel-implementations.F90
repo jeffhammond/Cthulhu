f3a37ce54bd90e6c624bb10a07297e634b259208
Jeff Hammond <jeff.science@gmail.com>
2022-08-30 14:47:19 +0300

    transpose works

--- transpose-kernel-implementations.F90
+++ transpose-kernel-implementations.F90
@@ -3,7 +3,7 @@ subroutine transpose_colon(block_order,TA,TB) bind(C)
     integer(kind=INT32), value :: block_order
     real(kind=REAL64), dimension(block_order,block_order) :: TA, TB
     integer(kind=INT32) :: i,j
-#if 1
+#if 0
     print*,'transpose_colon:',block_order
     print*,'transpose_colon: CONTIGUOUS:',is_contiguous(TA)
     print*,'transpose_colon: CONTIGUOUS:',is_contiguous(TB)
@@ -22,12 +22,7 @@ subroutine transpose_colon(block_order,TA,TB) bind(C)
     print*,'transpose_colon: ',TA(1,1)!,TA(block_order,block_order)
     print*,'transpose_colon: ',TB(1,1)!,TB(order,block_order)
 #endif
-    !do concurrent (j=1:block_order, i=1:block_order)
-    !  TB(i,j) = TB(i,j) + TA(j,i)
-    !enddo
-    do j=1,block_order
-      do i=1,block_order
-        TB(i,j) = TB(i,j) + TA(j,i)
-      enddo
+    do concurrent (j=1:block_order, i=1:block_order)
+      TB(i,j) = TB(i,j) + TA(j,i)
     enddo
 end subroutine transpose_colon
--- transpose-kernel-interfaces.F90
+++ transpose-kernel-interfaces.F90
@@ -4,7 +4,7 @@ module tki
         subroutine transpose_colon_trampoline(block_order,TA,TB) bind(C)
             use, intrinsic :: iso_fortran_env
             integer(kind=INT32), value :: block_order
-            real(kind=REAL64), dimension(block_order,block_order) :: TA,TB
+            real(kind=REAL64), dimension(:,:) :: TA,TB
         end subroutine transpose_colon_trampoline
     end interface
 end module tki
